355a42c1274a21d795b93ba05b974f42dea79b53
github username <github email address>
2018-10-21 00:20:32 -0400

    My first commit

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    array.join
+  elsif array.size == 2
+    array.join(" and ")
+  else array.size > 2
+    last_word = array.pop
+    oxford_phrase = array.join(", ")
+    oxford_phrase << ", and #{last_word}"
+  end
 end
